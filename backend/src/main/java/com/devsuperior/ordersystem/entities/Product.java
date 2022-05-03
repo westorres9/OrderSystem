b3f1ee91623268308de18a778d86982c06e1cd42
westorres9 <wester.torres9@gmail.com>
2022-05-02 23:49:21 -0300

    implementada classe product

---
+++ backend/src/main/java/com/devsuperior/ordersystem/entities/Product.java
@@ -0,0 +1,67 @@
+package com.devsuperior.ordersystem.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    private Long id;
+    private String name;
+    private Double price;
+
+    private List<Category> categories = new ArrayList<>();
+
+    public Product(){
+    }
+
+    public Product(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
